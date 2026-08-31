a9f82b8de3604f59c99c6d88351bfde2e43d987f
Flamewall <abbamahdi9@gmail.com>
2026-08-31 09:57:44 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
-$windows = "filename"; //zip
-$android = "filename"; //apk
-$tools = "toolspage";
-$demonlist = "demonlist";
-$youtube = "youtubeprofile";
-$discord = "discordserver";
-$about = "Write information about your GDPS and its features.";
+$windows = "https://drive.google.com/file/d/1acmYoes4l8fakMooHqk4VaglJdtyQpA7/view?usp=drivesdk"; //zip
+$android = "https://drive.google.com/file/d/1pXlkIMaI1O7fUubxzXaZnXYGEezqTbm8/view?usp=drivesdk"; //apk
+$tools = "https://avernuswall.ps.fhgdps.com/dashboard";
+$demonlist = "https://flamewallgdpsdemonlist.page.gd/";
+$youtube = "https://youtube.com/@flamewall-gdps?si=KhBq2Jrtxi9_Ew58";
+$discord = "https://discord.gg/EhERhENbw3";
+$about = "Low rating standards, 660+ levels, 130+ registered users, interesting contests, all in one gdps Inspired by Flamewall";
 ?>
